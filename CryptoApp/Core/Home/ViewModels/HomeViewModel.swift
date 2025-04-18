0b4cf53e2ea75ebd73a044a1c3afaba10b7b7758
Mehdi Ali <sendittomehdi@gmail.com>
2025-04-18 16:03:25 +0700

    Adding CoinRowView in HomeView
    Added CoinRowView inside HomeView. Had to create a HomeViewModel where all the coins will be downloaded using the CoinGecko API.

---
+++ CryptoApp/Core/Home/ViewModels/HomeViewModel.swift
@@ -0,0 +1,21 @@
+//
+//  HomeViewModel.swift
+//  CryptoApp
+//
+//  Created by Mehdi Ali on 18/4/25.
+//
+
+import Foundation
+
+class HomeViewModel: ObservableObject {
+    
+    @Published var allCoins: [CoinModel] = []
+    @Published var portfolioCoins: [CoinModel] = []
+    
+    init() {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+            self.allCoins.append(DeveloperPreview.instance.coin)
+            self.portfolioCoins.append(DeveloperPreview.instance.coin2)
+        }
+    }
+}
