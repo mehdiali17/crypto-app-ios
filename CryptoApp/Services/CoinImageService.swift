c9d860eff4ea8a97a2af7bafe06633bf38537125
Mehdi Ali <sendittomehdi@gmail.com>
2025-04-24 09:58:49 +0700

    FileManager added
    Added a file manager to save the app from downloading the coin icons all the time. Now the app only downloads the icons once and saves it in the file manager. Whenever needed, the icons just get loaded instead of being downloaded.

--- CryptoApp/Services/CoinImageService.swift
+++ CryptoApp/Services/CoinImageService.swift
@@ -14,13 +14,28 @@ class CoinImageService {
     @Published var image: UIImage? = nil
     private var imageSubscription: AnyCancellable?
     private let coin: CoinModel
+    private let fileManager = LocalFileManager.instance
+    private let folderName = "coin_images"
+    private let imageName: String
     
     init (coin: CoinModel) {
         self.coin = coin
+        self.imageName = coin.id
         getCoinImage()
     }
     
     private func getCoinImage() {
+        if let savedImage = fileManager.getImage(imageName: imageName, folderName: folderName) {
+            image = savedImage
+            print ("Retrived image from file manager.")
+        } else {
+            downloadCoinImage()
+            print ("Downloading image now.")
+            
+        }
+    }
+    
+    private func downloadCoinImage() {
         guard let url = URL(string: coin.image) else { return }
         
         imageSubscription = NetworkingManager.download(url: url)
@@ -28,8 +43,10 @@ class CoinImageService {
                 return UIImage(data: data)
             })
             .sink(receiveCompletion: NetworkingManager.handleCompletion, receiveValue: { [weak self] (returnedImage) in
-                self?.image = returnedImage
-                self?.imageSubscription?.cancel()
+                guard let self = self, let downloadedImage = returnedImage else {return}
+                self.image = downloadedImage
+                self.imageSubscription?.cancel()
+                self.fileManager.saveImage(image: downloadedImage, imageName: self.imageName, folderName: self.folderName)
             })
     }
 }
