7585160a002afe28816c989df3c31c44fea41257
Mehdi Ali <sendittomehdi@gmail.com>
2025-04-25 15:22:13 +0700

    Search bar now working
    Added code to make the search bar filter the coins.

--- CryptoApp/Core/Home/ViewModels/HomeViewModel.swift
+++ CryptoApp/Core/Home/ViewModels/HomeViewModel.swift
@@ -23,11 +23,23 @@ class HomeViewModel: ObservableObject {
     
     func addSubscribers() {
         
-        dataService.$allCoins
-            .sink { [weak self] returnedCoins in
+        $searchText
+            .combineLatest(dataService.$allCoins)
+            .debounce(for: .seconds(0.5), scheduler: DispatchQueue.main)
+            .map(filterCoins)
+            .sink { [weak self] (returnedCoins) in
                 self?.allCoins = returnedCoins
             }
             .store(in: &cancellables)
-        
+    }
+    
+    private func filterCoins(text: String, coins: [CoinModel]) -> [CoinModel] {
+        guard !text.isEmpty else {
+            return coins
+        }
+        let lowerCaseText = text.lowercased()
+        return coins.filter { (coin) -> Bool in
+            return coin.name.lowercased().contains(lowerCaseText) || coin.symbol.lowercased().contains(lowerCaseText) || coin.id.lowercased().contains(lowerCaseText)
+        }
     }
 }
