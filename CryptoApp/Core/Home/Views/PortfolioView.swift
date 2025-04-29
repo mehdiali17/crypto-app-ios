1a0f4b48075ff719ce94645633b5bbb55e30483f
Mehdi Ali <sendittomehdi@gmail.com>
2025-04-29 16:19:21 +0700

    Building Portfolio View

--- CryptoApp/Core/Home/Views/PortfolioView.swift
+++ CryptoApp/Core/Home/Views/PortfolioView.swift
@@ -9,11 +9,37 @@ import SwiftUI
 
 struct PortfolioView: View {
     
+    @EnvironmentObject private var vm: HomeViewModel
+    @State private var selectedCoin: CoinModel? = nil
+    
     var body: some View {
         NavigationView {
             ScrollView {
                 VStack(alignment: .leading, spacing: 0) {
-                    Text("Portfolio View")
+                    SearchBarView(searchText: $vm.searchText)
+                    ScrollView(.horizontal, showsIndicators:  false) {
+                        LazyHStack(spacing: 10) {
+                            ForEach(vm.allCoins) { coin in
+                                CoinLogoView(coin: coin)
+                                    .frame(width: 75)
+                                    .padding(4)
+                                    .onTapGesture {
+                                        withAnimation(.easeIn) {
+                                            selectedCoin = coin
+                                        }
+                                    }
+                                    .background(
+                                        RoundedRectangle(cornerRadius: 10)
+                                            .stroke(
+                                                selectedCoin?.id == coin.id ? Color.theme.green : Color.clear,
+                                                lineWidth: 1
+                                            )
+                                    )
+                            }
+                        }
+                        .padding(.vertical, 4)
+                        .padding(.leading)
+                    }
                 }
             }
             .navigationTitle("Edit Portfolio")
@@ -29,5 +55,6 @@ struct PortfolioView: View {
 struct PortfolioView_Previews: PreviewProvider {
     static var previews: some View {
         PortfolioView()
+            .environmentObject(dev.homeVM)
     }
 }
